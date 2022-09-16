1de58ef146592cf05d94be32138c18badc8f786b
Meriç Cebeci <mericcebeci@gmail.com>
2022-09-16 14:16:09 +0300

    İki Segue Olusturuldu Ve Koda Baglandi

---
+++ SegueApp/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  SegueApp
+//
+//  Created by Meriç Cebeci on 16.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var myLabelSecondViewController: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Meriç Cebeci on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
